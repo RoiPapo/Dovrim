351972a2512fc83136a27efe684d9930fd9e8a64
Roi Papo <thedarkhermit@gmail.com>
2017-05-08 17:58:28 +0300

    functionality from old version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-   loadedFeature = 'mydiscussion';
+   loadedFeature = 'home';
 
   onNavigate(feature: string) {
     this.loadedFeature = feature;
--- src/app/newdiscussion/newdiscussion.component.ts
+++ src/app/newdiscussion/newdiscussion.component.ts
@@ -1,15 +1,49 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-newdiscussion',
   templateUrl: './newdiscussion.component.html',
   styleUrls: ['./newdiscussion.component.css']
 })
-export class NewdiscussionComponent implements OnInit {
+export class NewdiscussionComponent  {
+ txtcontainer = document.getElementById("txtcontainer"); // the html div that contains subjects
+ texts = this.txtcontainer.getElementsByTagName("p");
 
   constructor() { }
 
-  ngOnInit() {
-  }
+ getValuesFromContainer(){
+   //todo: write a new get Value from container
+     var inputsitem = {
+        clocksarr:2 ,
+        times:2 ,
+    };
+    
+    return inputsitem;
+ }
+
+
+ submiting() { // creates json to please the lord node
+    var discussionName = "cat";
+    var textarr = [];
+    var subject = [];
+    var timetaker = this.getValuesFromContainer().clocksarr;
+    for (var i = 0; i < this.texts.length; i++) {
+        textarr.push(this.texts.item(i).innerHTML);
+    }
+
+    for (var i = 0; i < textarr.length; i++) {
+        subject.push({ subjectName: textarr[i], subjectTime: timetaker[i] })
+    }
+    var datafordb = {
+        discussionName: discussionName,
+        subject: subject
+    }
+
+    var xhr = new XMLHttpRequest();
+    xhr.open('POST', 'http://localhost:3000/api/status');   //server name patch papo
+    xhr.setRequestHeader('Content-Type', 'application/json; charset=utf-8');
+    xhr.send(JSON.stringify(datafordb));
+
+}
 
 }
