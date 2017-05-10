4ad6e9068c1ca1356591d46ab374bb41411f6dd9
Roi Papo <thedarkhermit@gmail.com>
2017-05-10 14:19:05 +0300

    Form Made

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule,FormBuilder,ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
@@ -20,7 +20,7 @@ import { RunComponent } from './run/run.component';
   imports: [
     BrowserModule,
     FormsModule,
-    FormsModule,
+    ReactiveFormsModule,
     HttpModule
   ],
   providers: [],
--- src/app/newdiscussion/newdiscussion.component.ts
+++ src/app/newdiscussion/newdiscussion.component.ts
@@ -1,32 +1,51 @@
-import { Component, Output } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormArray, FormBuilder, Validators } from '@angular/forms';
-import { Subject } from '../subject.model'
+import { Subject } from '../subject.model';
+import { Discussion } from '../discussion.model'
 
 @Component({
     selector: 'app-newdiscussion',
     templateUrl: './newdiscussion.component.html',
     styleUrls: ['./newdiscussion.component.css']
 })
-export class NewdiscussionComponent {
+export class NewdiscussionComponent implements OnInit {
     discussionName: string;
     inputs: Subject[] = [];
-    
-    constructor() { }
+    myForm: FormGroup;
+    constructor(private _fb: FormBuilder) { }
 
-    addLink() {
-        this.inputs.push({ name:"default" , time:10  });
+     ngOnInit() {
+        this.myForm = this._fb.group({
+            discussionName: ['', [Validators.required, Validators.minLength(3)]],
+            subjects: this._fb.array([
+                this.initSubject(),
+            ])
+        });
     }
 
-    // getValuesFromContainer() {
-    //     //todo: write a new get Value from container
-    //     var inputsitem = {
-    //         clocksarr: 2,
-    //         times: 2,
-    //     };
+     initSubject() {
+        return this._fb.group({
+            subjectName: ['', Validators.required],
+            subjectTime: ['']
+        });
+    }
 
-    //     return inputsitem;
-    // }
+    addLink() {
+        const control = <FormArray>this.myForm.controls['subjects'];
+        control.push(this.initSubject());
+    }
 
+    removesubjects(i: number) {
+        const control = <FormArray>this.myForm.controls['subjects'];
+        control.removeAt(i);
+    }
+    save(model: FormGroup) {
+        // call API to save
+        // ...
+        console.log(model.value);
+    }
+
+   
 
     // submiting() { // creates json to please the lord node
     //     var discussionName = "cat";
